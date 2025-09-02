9af6c10f71d803389088d6431311cbc0aeb6bead
Devarsh <devarsh.chhatrala@gmail.com>
2025-09-02 23:11:18 +0530

    updating debug and allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -27,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2b0m5z__m%(id@*ehzz1gl6wm^z7xv8vs8&u+su!6ef9nl4l^-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+# Update allowed hosts before final release
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
